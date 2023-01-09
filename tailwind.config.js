012f208611524afaf92091d5003c0131638b3ad9
manhtai <vo@manhtai.com>
2023-01-09 20:32:26 +0700

    TnC (#4)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["*.html"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
